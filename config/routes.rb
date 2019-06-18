9f7a0b49556853bcd0c5749d8745b389fdcadf21
hogehoge <AAAgmail.com>
2019-06-18 18:01:18 +0900

    routeを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,22 @@
 Rails.application.routes.draw do
   root "home#top"
   get 'home/top'
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-
 
   devise_for :users, controllers: {
-        registrations: 'users/registrations',
-        sessions: 'users/sessions'
-}
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
 
+  get 'users/show'
+  resources :users, only: [:show]
 
+  resources :events
+    root 'events#index'
+  end
 
-resources :users, only: [:show]
-get 'users/show'
 
 devise_scope :user do
   get '/users/sign_out' => 'devise/sessions#destroy'
 end
 
-resources :events
-  root 'events#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+
